9227729f1832ca5c6b68e7054731e6ab38359b5f
azyz78 <midousaid26@gmail.com>
2020-01-27 23:55:38 +0100

    todo

---
+++ src/App.js
@@ -0,0 +1,16 @@
+  import React from 'react';
+  import './App.css';
+  import Todo from './Todo'
+
+
+
+  function App() {
+    return (
+      <div className='todo'>
+        <h1>Daily Todo Lists</h1>
+        <Todo/>
+      </div>
+    );
+  }
+
+  export default App;
